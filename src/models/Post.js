c9c4aa61d772ea72bc548ab54a6a4a396113b18d
Daniel de Souza Neri <danielneris01@gmail.com>
2019-07-16 22:33:19 -0300

    ajuste

--- src/models/Post.js
+++ src/models/Post.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const PostSchema = new mongoose.Schema({
     author: String,
     place: String,
-    descripton: String,
+    description: String,
     hashtags: String,
     image: String,
     likes: {
